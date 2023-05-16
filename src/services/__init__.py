cb260e7f076a76064a0e466577e8f7b0bf7aad8f
Ivan Bazhenov <bazhenov.in@gmail.com>
2023-05-16 22:50:20 +0300

    [#41] Добавление абстракций

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -5,6 +5,7 @@ from elasticsearch import AsyncElasticsearch
 from redis.asyncio import Redis
 
 from constants import CACHE_TIME
+from abc import ABC, abstractmethod
 
 logger = logging.getLogger(__name__)
 
@@ -25,3 +26,50 @@ class BaseService:
 
         logger.info(f'Получение данных из кеша: key = {key}')
         return json_data
+
+
+class AbstractCache(ABC):
+    @abstractmethod
+    async def put_to_cache(self, key, data):
+        """Метод записи в кеш.
+        """
+        pass
+
+    @abstractmethod
+    async def get_from_cache(self, key):
+        """Метод получения данных из кеша.
+        """
+        pass
+
+
+class AbstractDBRequest(ABC):
+    @abstractmethod
+    async def get_by_id(self, id_: str):
+        """Метод получения объекта по id.
+        """
+        pass
+
+    @abstractmethod
+    async def filter(self, **kwargs):
+        """Метод получения списка объектов по параметрам.
+        """
+        pass
+
+
+class AbstractService(ABC):
+    @abstractmethod
+    def __init__(self, cache: AbstractCache, request: AbstractDBRequest):
+        self.cache = cache
+        self.request = request
+
+    @abstractmethod
+    async def get_by_id(self, id_: str):
+        """Метод получения объекта по id.
+        """
+        pass
+
+    @abstractmethod
+    async def filter(self, **kwargs):
+        """Метод получения списка объектов по параметрам.
+        """
+        pass
