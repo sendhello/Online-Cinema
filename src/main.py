d0ffae39d8a07aaf1ab9eff2615dba778520b4ab
Ivan Bazhenov <bazhenov.in@gmail.com>
2023-05-23 18:30:01 +0300

    [#60] Использование lifespan при запуске и остановке приложения

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
@@ -7,7 +9,19 @@ from api import router as api_router
 from core.config import settings
 from db import elastic, redis
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis.redis = Redis(host=settings.redis_host, port=settings.redis_port)
+    elastic.es = AsyncElasticsearch(hosts=[f'http://{settings.elastic_host}:{settings.elastic_port}'])
+    yield
+
+    await redis.redis.close()
+    await elastic.es.close()
+
+
 app = FastAPI(
+    lifespan=lifespan,
     title=settings.project_name,
     description="Информация о фильмах, жанрах и людях, участвовавших в создании произведения",
     version="1.0.0",
@@ -16,19 +30,4 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
 )
 
-
-@app.on_event('startup')
-async def startup():
-    redis.redis = Redis(host=settings.redis_host, port=settings.redis_port)
-    elastic.es = AsyncElasticsearch(hosts=[f'http://{settings.elastic_host}:{settings.elastic_port}'])
-
-
-@app.on_event('shutdown')
-async def shutdown():
-    await redis.redis.close()
-    await elastic.es.close()
-
-
-# Подключаем роутер к серверу, указав префикс /v1/films
-# Теги указываем для удобства навигации по документации
 app.include_router(api_router, prefix='/api')
