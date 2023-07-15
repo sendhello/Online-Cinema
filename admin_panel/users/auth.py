715982c1bfe537e59d89a270d6549355541bcf7a
Ivan Bazhenov <ivan.bazhenov@balance-pl.ru>
2023-07-15 17:45:43 +0300

    [admin] Добавление мягкой деградации сервиса Auth для Admin Panel

--- admin_panel/users/auth.py
+++ admin_panel/users/auth.py
@@ -17,27 +17,36 @@ class CustomBackend(BaseBackend):
         url = settings.AUTH_API_LOGIN_URL
         payload = {'email': username, 'password': password}
         response = requests.post(url, data=json.dumps(payload))
-        if response.status_code != http.HTTPStatus.OK:
-            return None
 
-        data = response.json()
+        if response.status_code == http.HTTPStatus.OK:
+            data = response.json()
 
-        try:
-            user, created = User.objects.get_or_create(
-                id=data['id'],
-            )
-            user.email = data.get('email')
-            user.first_name = data.get('first_name')
-            user.last_name = data.get('last_name')
-            user.is_admin = data.get('role') == Roles.ADMIN
-            user.is_staff = user.is_admin
-            user.is_active = True
-            user.save()
+            try:
+                user, created = User.objects.get_or_create(id=data['id'])
+                user.set_password(password)
+                user.email = data.get('email')
+                user.first_name = data.get('first_name')
+                user.last_name = data.get('last_name')
+                user.is_admin = data.get('role') == Roles.ADMIN
+                user.is_staff = user.is_admin
+                user.is_active = True
+                user.save()
 
-        except Exception:
-            return None
+            except Exception:
+                return None
 
-        return user
+            return user
+
+        else:
+            # Если не получилось получить данные по пользователю из внешней аутентификации,
+            # пробуем взять данные из собственной БД
+            try:
+                user = User.objects.get(email=username)
+                if user and user.check_password(password):
+                    return user
+
+            except Exception:
+                return None
 
     def get_user(self, user_id):
         try:
